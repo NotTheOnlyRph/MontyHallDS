c44807c04b656fdd14a812b6fb55187a13708d95
NotTheOnlyRph <raphael.stoltz2009@gmail.com>
2025-01-02 10:09:41 +0100

    Highlighted door size fix

--- Assets/Grit/grit.sh
+++ Assets/Grit/grit.sh
@@ -4,13 +4,11 @@ GRIT=/opt/blocksds/core/tools/grit/grit
 
 # Edit here your sprite files
 
-$GRIT CurrentDoorBottom.png -ftB -fh! -gTFF00FF -gt -gB8 -m!
-$GRIT CurrentDoorTop.png -ftB -fh! -gTFF00FF -gt -gB8 -m!
 $GRIT UsedDoorBottom.png -ftB -fh! -gTFF00FF -gt -gB8 -m!
 $GRIT UsedDoorTop.png -ftB -fh! -gTFF00FF -gt -gB8 -m!
 
 # Edit here your BG files
 
-$GRIT ChangeDoor.png -ftB -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
-$GRIT Doors.png -ftB -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
+#$GRIT ChangeDoor.png -ftB -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
+#$GRIT Doors.png -ftB -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
 
